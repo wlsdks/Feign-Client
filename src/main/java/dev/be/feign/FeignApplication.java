4de1865b971091f911d8fc42f33f9bd6abae894a
jinan <ja.choi@sweetk.co.kr>
2023-04-23 14:45:47 +0900

    1. feignClient를 사용하기 위해 main에 어노테이션 선언추가

--- src/main/java/dev/be/feign/FeignApplication.java
+++ src/main/java/dev/be/feign/FeignApplication.java
@@ -2,7 +2,9 @@ package dev.be.feign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class FeignApplication {
 
